63182c132c121aab13361f2904e8be4957cf42c0
Gurpreetkaur <Gurpreetkaur@lambton college>
2015-09-15 16:12:45 -0400

    Java Development Assign1

--- src/helloworld2014/HelloWorld2014.java
+++ src/helloworld2014/HelloWorld2014.java
@@ -11,6 +11,35 @@ public class HelloWorld2014 {
      */
     public static void main(String[] args) {
         System.out.println("Hello lambton!");
+        /***************
+         * 
+         * Assignment 1
+         * 
+         * Job Posting
+         * Java/Front-end Developer
+         * TapAnalytics-Montreal,QC
+         * http://www.indeed.ca/cmp/TapAnalytics/jobs/Java-c7f317241a76c443?sjdu=vQIlM60yK_PwYat7ToXhk14DjfOtszabPHaHNqvv3ukfLWribLbVi-1E281KbGl6NOpmuMbgXCcvi_3GditoK8XngtCWqOtddDdXQLWPZe8
+         *
+         *
+         * 
+         * Skills
+         * 
+         * Javascript -dynamic proramming language-https://en.wikipedia.org/wiki/JavaScript
+         * Angular-open source web application framework-https://en.wikipedia.org/wiki/JavaScript
+         * Rest-software architecture style-https://en.wikipedia.org/wiki/JavaScript
+         * XML-markup language-https://en.wikipedia.org/wiki/JavaScript
+         * JSON-open standard-https://en.wikipedia.org/wiki/JavaScript
+         * AJAX-asynchronous web application-https://en.wikipedia.org/wiki/JavaScript
+         * 
+         * 
+         * 
+         * Salary
+         * 
+         * Toronto- $89k
+         * Vancouver-$79k
+         * Waterloo-$76k
+         * 
+         */
     }
     
 }
